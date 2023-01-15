5a7c576452901ba488806a8812ef67babfd2bc94
Chris Athanas <realityexpander@gmail.com>
2023-01-15 01:28:26 -0600

    Improve comments

--- app/src/main/java/com/realityexpander/composeuioptimizationprep/optimization2/WelcomeView.kt
+++ app/src/main/java/com/realityexpander/composeuioptimizationprep/optimization2/WelcomeView.kt
@@ -26,24 +26,24 @@ fun WelcomeViewAlternative(
 }
 
 // Option 2
-// Use a @Stable object
+// Use a mapped class from this module
 @Composable
 fun WelcomeViewAlternative2(
+    user: User
+) {
+    Text(text = "Welcome ${user.username}!")
+}
+
+// Option 3
+// Use a @Stable object
+@Composable
+fun WelcomeViewAlternative3(
     user: ExternalUser
 ) {
     val username by remember(user) {
         mutableStateOf(user.username)
     }
     Text(text = "Welcome $username!")
-}
-
-// Option 2
-// Use a mapped class from this module
-@Composable
-fun WelcomeViewAlternative3(
-    user: User
-) {
-    Text(text = "Welcome ${user.username}!")
 }
 
 @Composable
@@ -71,8 +71,12 @@ fun Optimization3(
 //        WelcomeViewAlternative(username = user2.username)
 
         // Option 2 - recomposes only when MAPPED user changes
-        WelcomeViewAlternative3(user.toUser())
-        WelcomeViewAlternative3(user2.toUser())
+//        WelcomeViewAlternative2(user.toUser())
+//        WelcomeViewAlternative2(user2.toUser())
+
+        // Option 3 - Uses remember to store username in a local @Stable object
+        WelcomeViewAlternative3(user)
+        WelcomeViewAlternative3(user2) // note the component will recompose but the text will not
 
         Button(onClick = {
             user = ExternalUser(
